99513fdc85c919f88d8a410a209e9f6ca089959e
Sadik.Ozer <Sadik.Ozer@maximintegrated.com>
2021-10-01 14:06:11 +0300

    Update uart interrupt example
    Signed-off-by: Sadik.Ozer <Sadik.Ozer@maximintegrated.com>

--- src/serial_irq.cpp
+++ src/serial_irq.cpp
@@ -7,26 +7,29 @@
 static DigitalOut led(LED1);
 
 // Create a UnbufferedSerial object with a default baud rate.
-static UnbufferedSerial  serial_port(UART0_TX, UART0_RX, 9600);
+static UnbufferedSerial  serial_port(UART0_TX, UART0_RX, 115200);
+static UnbufferedSerial  debug_port(UART1A_TX, UART1A_RX, 115200);
+
+static char g_buf[512];
+static int  g_rxLen;
 
 static void on_rx_interrupt(void)
 {
-    char c;
-
     // Toggle the LED.
-    led = !led;
+    //led = !led;
 
     // Read the data to clear the receive interrupt.
-    if (serial_port.read(&c, 1)) {
-        // Echo the input back to the terminal.
-        serial_port.write(&c, 1);
+    g_rxLen = serial_port.read(g_buf, sizeof(g_buf));
+    if (g_rxLen > 0) {
+        // send over debug port
+        debug_port.write(g_buf, g_rxLen);
     }
 }
 
 int test_serial_irq(void)
 {
-    // Set desired properties (9600-8-N-1).
-    serial_port.baud(9600);
+    // Set desired properties (115200-8-N-1).
+    serial_port.baud(115200);
     serial_port.format(
         /* bits */ 8,
         /* parity */ SerialBase::None,
